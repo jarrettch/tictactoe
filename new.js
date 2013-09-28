d14ae8b9aec07ee23abf260fe4bc4f861a64dfa2
jarrettch <jarrett.harris@gmail.com>
2013-09-28 09:40:53 -0700

    Added array method

--- new.js
+++ new.js
@@ -1,9 +1,33 @@
 
 var turn = 0;
+var board = [[null,null,null],[null,null,null],[null,null,null],]
 
+function playBox(box) { //prevents turn++ affecting X-O placement
+	if(box.innerHTML != "")
+		return;
 
-function playBox(box) {
-	box.innerHTML = (turn % 2 == 0) ? 'X' : 'O';
+	box.innerHTML = (turn % 2 == 0) ? 'X' : 'O'; //alternates X-O
 	turn++;
-	Object.freeze(box);
+	Object.freeze(box); //prevents Xs and Os from being changed
+
+	var moveLetter;
+	if (turn%2 == 0)
+	moveLetter = 'O';
+	else
+	moveLetter = "X";
+	box.value = moveLetter; //Me: not sure what this line does
+							//works when omitted
+
+	// Place this move in the array
+	// Here's the version that works if you are using cell1_3 for the ID
+	// board[(event.target.id[4]) - 1][(event.target.id[6]) - 1]
+	// = moveLetter;
+
+	// Here's the version that works if you're using cell5 for the ID
+	board
+	[Math.floor(((event.target.id[4]) - 1) / 3)]
+	[((event.target.id[4]) - 1) % 3]
+	= moveLetter;
+
+
 }
